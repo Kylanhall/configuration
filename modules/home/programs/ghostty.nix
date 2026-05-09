677642a83c901ce5f9c7e172b012a35507c91639
kylanhall <git@kylan.net>
2026-05-09 04:50:04 -0400

    added term = xterm-256color for ssh support

--- modules/home/programs/ghostty.nix
+++ modules/home/programs/ghostty.nix
@@ -3,14 +3,15 @@
 {
   xdg.configFile."ghostty/config".text = ''
     font-family = JetBrains Mono
-    font-size = 12
+    font-size = 10
     theme = Dracula
     background-opacity = 1.0
     cursor-style = block
     mouse-hide-while-typing = true
     shell-integration = zsh
-    window-padding-x = 12
-    window-padding-y = 12
+    window-padding-x = 6
+    window-padding-y = 6
     macos-option-as-alt = true
+    term = xterm-256color
   '';
 }
