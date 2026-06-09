47404a4348714c18baf7a893314302d4e3fb7015
Kylan <git@kylan.net>
2026-06-08 20:39:37 -0400

    Updated secrets.nix to reflect current secrets file

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,14 @@
 {
-  username = "";
-  gitUser = "";
-  gitEmail = "";
-  sshKeyName = "";
+    username = "";
+    sshKeyName = "";
+    weatherLocation = "";
+    sshKeyName2 = "";
+    
+    github1 = "";
+    git1User = "";
+    git1Email = "";
+
+    github2 = "";
+    git2User = "";
+    git2Email = "";
 }
